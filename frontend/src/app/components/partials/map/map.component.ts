cbc58744e1979f60617196923a72673e792f76f2
Abhishek Dash <abhishek953dash@gmail.com>
2023-07-13 14:26:04 +0530

    payment page component

--- frontend/src/app/components/partials/map/map.component.ts
+++ frontend/src/app/components/partials/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, ViewChild } from '@angular/core';
 import { LatLng, LatLngExpression, LatLngTuple, LeafletEvent, LeafletMouseEvent, Map, Marker, icon, map, marker, tileLayer } from 'leaflet';
 import { LocationService } from 'src/app/services/location.service';
 import { Order } from 'src/app/shared/models/Order';
@@ -8,10 +8,11 @@ import { Order } from 'src/app/shared/models/Order';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit{
-    @Input() order!:Order;
-    private readonly DEFAULT_LATLNG:LatLngTuple = [13.75,21.62];
-    private readonly MARKER_ZOOM_LEVEL = 16;
+export class MapComponent implements OnChanges{
+  @Input() order!:Order;
+  @Input() readonly = false;
+  private readonly DEFAULT_LATLNG:LatLngTuple = [13.75,21.62];
+  private readonly MARKER_ZOOM_LEVEL = 16;
     private readonly MARKER_ICON = icon({
         iconUrl:
           'https://res.cloudinary.com/foodmine/image/upload/v1638842791/map/marker_kbua9q.png',
@@ -25,9 +26,29 @@ export class MapComponent implements OnInit{
     currentMarker!:Marker;
 
     constructor(private locationService:LocationService){}
-    ngOnInit(): void {
-        this.initializeMap()
+    ngOnChanges(): void {
+        if(!this.order) return;
+        this.initializeMap();
+
+        if(this.readonly && this.addressLatLng){
+            this.showLocationOnReadonlyMode();
+        }
     }
+  showLocationOnReadonlyMode() {
+    const m = this.map;
+    this.setMarker(this.addressLatLng);
+    m.setView(this.addressLatLng,this.MARKER_ZOOM_LEVEL);
+
+    m.dragging.disable();
+    m.touchZoom.disable();
+    m.doubleClickZoom.disable();
+    m.scrollWheelZoom.disable();
+    m.boxZoom.disable();
+    m.keyboard.disable();
+    m.off('click');
+    m.tap?.disable();
+    this.currentMarker.dragging?.disable();
+  }
 
     initializeMap(){
         if(this.map) return;
@@ -72,10 +93,15 @@ export class MapComponent implements OnInit{
     }
 
     set addressLatLng(latlng:LatLng){
+        if(!latlng.lat.toFixed) return;
+
         latlng.lat = parseFloat(latlng.lat.toFixed(8));
         latlng.lng = parseFloat(latlng.lng.toFixed(8));
         this.order.addressLatLng = latlng;
         console.log(this.order.addressLatLng);
+    }
 
+    get addressLatLng(){
+      return this.order.addressLatLng!;
     }
 }
