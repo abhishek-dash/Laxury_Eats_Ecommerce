084bb3749b681ae57b6573c7f4fee0026093bc13
Abhishek Dash <abhishek953dash@gmail.com>
2023-08-13 22:11:53 +0530

    deploy on render fixing Cors issue

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,6 +13,11 @@ dbConnect();
 const app = express();
 app.use(express.json());
 
+app.use(cors({
+  credentials:true,
+  origin:["http://localhost:4200"]
+}))
+
 
 app.use("/api/foods",foodRouter);
 app.use("/api/users",userRouter);
